d7d1efde4b093ffcd3f554e1c8f32571a3d5834b
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-03-18 17:33:00 -0400

    model paciente

---
+++ src/models/pacienteModel.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Paciente = sequelize.define('Paciente' ,{
+    id_user: {
+        type: DataTypes.STRING,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    data_nascimento: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    cpf:{
+        type: DataTypes.STRING(14),
+        allowNull: false,
+        validate:{
+            is: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+        }
+    },
+    telefone: {
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    email:{
+        type: DataTypes.STRING,
+    },
+    id_endereco: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: 'enderecoModel',
+            key: 'id'
+        }
+    }
+});
+
+module.exports = Paciente
