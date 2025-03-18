ce3ce331be889e8f594480c821da2d124fe41e48
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-03-18 17:33:16 -0400

    model profissional

---
+++ src/models/profissionalModel.js
@@ -0,0 +1,50 @@
+const { DataTypes } =  require('sequelize');
+const sequelize = require('../database');
+
+const Profissional = sequelize.define('Profissional', {
+    id_profissional: {
+        type: DataTypes.STRING,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cpf:{
+        type: DataTypes.STRING(14),
+        allowNull: false,
+        validate:{
+            is: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+        }
+    },
+    data_nascimento:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    telefone:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+    },
+    id_endereco:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model:'enderecoModel',
+            key: 'id_endereco'
+        }
+    },
+    cargo:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    status:{
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+
+})
+
+module.exports = Profissional
