5b7f9b66fc9377467848c0a7e484c6f3d93e0be8
Your Name <you@example.com>
2025-03-18 21:16:20 -0400

    correção do arquivo

--- src/adpters/express-route-adapter.js
+++ src/adpters/express-route-adapter.js
@@ -1,13 +1,10 @@
-const { Request, Response } = require('express');
-const { Controller, HttpRequest } = require('./protocols');
-
 const adaptRoute = (controller) => {
     return async function (req, res) {
         const httpRequest= {
             body: req.body
         };
         const httpResponse = await controller.handle(httpRequest);
-        res.status(httpRequest.statusCode).json(httpResponse.body);
+        res.status(httpResponse.statusCode).json(httpResponse.body);
     };
 };
 
