45255d6d9051a2a1e47a451db5218189e3503aef
Your Name <you@example.com>
2025-03-19 21:34:52 -0400

    alteração na controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const User = require('../models/userModel');
 const router = express.Router();
-const routerAdapter = require('../adpters/express-route-adapter')
-const CriarUsuarioController = require('../controllers/criar-usuarios')
+const User = require('../models/userModel');
+const routeAdapter = require('../adapters/express-route-adapters');
+const CriarUsuarioController = require('../controllers/criar-usuarios');
+const ListarUsuarioController = require('../controllers/listar-usuarios');
+const EditarUsuarioController = require('../controllers/editar-usuario');
+const DeletarUsuarioController = require('../controllers/deletar-usuario');
+const adaptRoute = require('../adapters/express-route-adapters');
+
 
 /**
  * @swagger
@@ -11,14 +16,10 @@ const CriarUsuarioController = require('../controllers/criar-usuarios')
  *     User:
  *       type: object
  *       required:
- *         - id
  *         - nome
  *         - senha
  *         - email
  *       properties:
- *         id:
- *           type: string
- *           description: O id é gerado automaticamente
  *         nome:
  *           type: string
  *           description: O nome de usuário
@@ -30,9 +31,9 @@ const CriarUsuarioController = require('../controllers/criar-usuarios')
  *           description: O email do usuário
  *       example:
  *         id: 1
- *         nome: Jose Cleber
- *         senha: 321a
- *         email: joaoCarlos@dominio.com
+ *         nome: João da Silva
+ *         senha: 123abc
+ *         email: joao.silva@dominio.com
  */
 
 /**
@@ -55,56 +56,44 @@ const CriarUsuarioController = require('../controllers/criar-usuarios')
  *           schema:
  *             $ref: '#/components/schemas/User'
  *     responses:
- *      201:
- *        description: O usuario foi criado com sucesso
- *      500:
- *        description: Algum erro aconteceu
- * 
+ *       201:
+ *         description: O usuário foi criado com sucesso!
+ *       500:
+ *         description: Algum erro aconteceu
  */
+router.post('/users', routeAdapter(new CriarUsuarioController()));
 
-// Endpoint para criação de recurso
-router.post('/users', routerAdapter(new CriarUsuarioController()));
 /**
  * @swagger
  * /api/users:
  *   get:
- *     summary: Retund the list of the users
+ *     summary: Retorna a lista de usuários
  *     tags: [Users]
  *     responses:
  *       200:
- *         description: The list of the users
+ *         description: A lista de usuários foi retornada com sucesso
  *         content:
  *           application/json:
- *              schema:
- *                type: array
- *                items:
- *                  $ref: '#/components/schemas/User'
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
  */
-
-
-// Endpoint para listagem de recursos
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.findAll();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+router.get('/users', routeAdapter(new ListarUsuarioController()));
 
 /**
  * @swagger
  * /api/users/{id}:
  *   put:
- *     summary: Upadate the user by the id
+ *     summary: Atualiza o usuário por id
  *     tags: [Users]
  *     parameters:
  *       - in: path
- *         nome: id
+ *         name: id
  *         schema:
- *           type: string
+ *           type: integer
  *         required: true
- *         description: the user id
+ *         description: The user id
  *     requestBody:
  *       required: true
  *       content:
@@ -113,61 +102,35 @@ router.get('/users', async (req, res) => {
  *             $ref: '#/components/schemas/User'
  *     responses:
  *       200:
- *         description: The user was updated
+ *         description: O usuário foi atualizado com sucesso
  *       404:
- *         description: The user was not found
+ *         description: O usuário não foi encontrado
  *       500:
- *         description: Some error happened
+ *         description: Algum erro aconteceu
  */
+router.put('/users/:id', adaptRoute(new EditarUsuarioController()));
 
-// Endpoint para atualização de recurso
-router.put('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByPk(req.params.id);
-        if (user) {
-            await user.update(req.body);
-            res.json(user);
-        } else {
-            res.status(404).json({ error: 'User not found' });
-        }
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
-/** 
+/**
  * @swagger
  * /api/users/{id}:
  *   delete:
- *     summary: Remove the user by id
+ *     summary: Remove o usuário por id
  *     tags: [Users]
  *     parameters:
  *       - in: path
- *         nome: id
+ *         name: id
  *         schema:
- *           type: string
+ *           type: integer
  *         required: true
- *         description: the user id
+ *         description: O id do usuário
  *     responses:
  *       200:
- *         description: The user was deleted
+ *         description: O usuário foi removido com sucesso
  *       404:
- *         description: The user was not found
- *    
+ *         description: O usuário não foi encontrado
+ *       500:
+ *         description: Algum erro aconteceu
  */
-
-// Endpoint para deletar recurso
-router.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByPk(req.params.id);
-        if (user) {
-            await user.destroy();
-            res.json({ message: 'User deleted' });
-        } else {
-            res.status(404).json({ error: 'User not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+router.delete('/users/:id', adaptRoute(new DeletarUsuarioController()));
 
 module.exports = router;
---
+++ src/routes/userRoutes_.js
@@ -0,0 +1,164 @@
+const express = require('express');
+const User = require('../models/userModel');
+const router = express.Router();
+const routerAdapter = require('../adapters/express-route-adapters')
+const CriarUsuarioController = require('../controllers/criar-usuarios')
+const ListarUsuarioController = require('../controllers/listar-usuarios')
+const AtualizaUsuariosController = require('../controllers/editar-usuario')
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - id
+ *         - nome
+ *         - senha
+ *         - email
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: O id é gerado automaticamente
+ *         nome:
+ *           type: string
+ *           description: O nome de usuário
+ *         senha:
+ *           type: string
+ *           description: A senha do usuário
+ *         email:
+ *           type: string
+ *           description: O email do usuário
+ *       example:
+ *         id: 1
+ *         nome: Jose Cleber
+ *         senha: 321a
+ *         email: joaoCarlos@dominio.com
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: Gerenciamento de usuários API
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Cria um novo usuário
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *      201:
+ *        description: O usuario foi criado com sucesso
+ *      500:
+ *        description: Algum erro aconteceu
+ * 
+ */
+
+// Endpoint para criação de recurso
+router.post('/users', routerAdapter(new CriarUsuarioController()));
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Retund the list of the users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of the users
+ *         content:
+ *           application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/User'
+ */
+
+
+// Endpoint para listagem de recursos
+router.get('/users', routerAdapter(new ListarUsuarioController()));
+// router.get('/users', async (req, res) => {
+//     try {
+//         const users = await User.findAll();
+//         res.json(users);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Upadate the user by the id
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         nome: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: the user id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user was updated
+ *       404:
+ *         description: The user was not found
+ *       500:
+ *         description: Some error happened
+ */
+
+// Endpoint para atualização de recurso
+router.put('/users/:id', routerAdapter(new AtualizaUsuariosController()))
+
+/** 
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Remove the user by id
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         nome: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: the user id
+ *     responses:
+ *       200:
+ *         description: The user was deleted
+ *       404:
+ *         description: The user was not found
+ *    
+ */
+
+// Endpoint para deletar recurso
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            await user.destroy();
+            res.json({ message: 'User deleted' });
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
