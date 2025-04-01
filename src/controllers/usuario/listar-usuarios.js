00a2973a4c647cd77ad7b8536b0ccac11e68731a
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-04-01 17:12:45 -0400

    correçã

--- src/controllers/usuario/listar-usuarios.js
+++ src/controllers/usuario/listar-usuarios.js
@@ -2,8 +2,22 @@ const { json } = require('sequelize');
 const User = require('../../models/userModel');
 
 class ListarUsuariosController {
-    async handle(httpRequest) {
+    async handle(req, res) {
         try {
+            const userId = req.parms?.id;
+            const usuario = await User.findByPk(userId)
+            if (!usuario && userId){
+                return{
+                    statusCode: 404,
+                    body: {error: "usuario nao encontrado"}
+                };
+            }else if(userId){
+                return{
+                    statusCode: 200,
+                    body: usuario
+                };
+            }
+
             const users = await User.findAll();
             return {
                 statusCode: 200,
