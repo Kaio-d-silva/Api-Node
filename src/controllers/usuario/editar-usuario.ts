379c733520fe46feebc7ecd763c6c54a12e305b8
Your Name <you@example.com>
2025-05-28 21:24:12 -0400

    validações

--- src/controllers/usuario/editar-usuario.ts
+++ src/controllers/usuario/editar-usuario.ts
@@ -1,9 +1,36 @@
 import { Controller, HttpRequest, HttpResponse } from '../../interfaces';
 import User from '../../models/user-model';
+import validator from 'validator';
+
 class EditarUsuarioController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { id } = httpRequest.params;
     const { nome, email, senha } = httpRequest.body;
+
+    const user = await User.findOne({ where: { id } });
+    if (!user){
+      return {
+        statusCode: 404,
+        body: { error: 'Usuário não encontrado' },
+      };
+    }
+    
+    // Validação dos dados de entrada
+    if (nome.length < 3) {
+      return {
+        statusCode: 400,
+        body: { error: 'Nome deve ter pelo menos 3 caracteres' },
+      };      
+    }
+
+
+    if (senha.length === null) {
+      return {
+        statusCode: 400,
+        body: { error: 'Senha não pode ser vazia' },
+      };
+    }
+
     try {
       const usuario = await User.findByPk(id);
       if (!usuario) {
