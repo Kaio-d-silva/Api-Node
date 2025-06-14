91bff8f1729e4220fe87f4c0fa065793ac003caa
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-06-14 13:57:18 -0400

    correção listagem endereço

--- src/controllers/endereco/listar-endereco.ts
+++ src/controllers/endereco/listar-endereco.ts
@@ -1,34 +1,34 @@
 import { Controller, HttpRequest, HttpResponse } from "../../interfaces";
 import Endereco from "../../models/enderecoModel";
+import enderecoRoutes from "../../routes/enderecoRoutes";
 
-class ListarEnderecoController implements Controller{
-  async handle(httpRequest:HttpRequest): Promise<HttpResponse> {
+class ListarEnderecoController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const enderecoId = httpRequest.params.id
-      console.log(`O id do endereço é ${enderecoId}`)
-      if(enderecoId){
-        
-        const endereco = await Endereco.findByPk(enderecoId);
-        
-        if(!endereco){
-          return{
-            statusCode: 404,
-            body: { error: "endereço não encontrado"}
-          }
-        }
+      const endereco = await Endereco.findByPk(enderecoId);
 
-        return{
-          statusCode: 200,
-          body: endereco
-        }
-      } else {
-        const enderecos = await Endereco.findAll();
+
+      console.log(`O id do endereço é ${enderecoId}`)
+
+      if (!endereco && enderecoId !== "{id}") {
         return {
-        statusCode: 200,
-        body: enderecos,
-      };
+          statusCode: 404,
+          body: { error: "endereço não encontrado" }
+        }
+      } else if (enderecoId !== '{id}') {
+        return {
+          statusCode: 200,
+          body: endereco,
+        };
       }
-    } catch (error:any) {
+      const enderecos = await Endereco.findAll();
+      return{
+        statusCode: 200,
+        body: enderecos
+      }
+
+    } catch (error: any) {
       return {
         statusCode: 500,
         body: { erro: error.message },
