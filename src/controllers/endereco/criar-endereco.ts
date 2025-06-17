eb0a063f6c5325739d562a6365ad9869cb653f8c
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-06-16 23:52:16 -0400

    tratativa paciente não encontrado

--- src/controllers/endereco/criar-endereco.ts
+++ src/controllers/endereco/criar-endereco.ts
@@ -1,11 +1,21 @@
 import Endereco from '../../models/enderecoModel';
 import { Controller, HttpRequest, HttpResponse } from '../../interfaces';
+import Paciente from '../../models/pacienteModel';
+import { error } from 'console';
 
 class CriaEnderecoController implements Controller{
   async handle(httpRequest:HttpRequest): Promise<HttpResponse> {
     try {
       const { logradouro, numero, complemento, bairro, cidade, estado, cep, id_paciente } =
         httpRequest.body;
+
+      const paciente = await Paciente.findByPk(id_paciente)
+      if (!paciente){
+        return{
+          statusCode: 404,
+          body: "Paciente não encontrado"
+        }
+      }
 
       const endereco = await Endereco.create({
         logradouro,
