486422ca56ba93d68805347163f2d123da6599ce
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-09-06 22:52:56 -0400

    testes usuario

---
+++ tests/usuario.test.ts
@@ -0,0 +1,61 @@
+import ListarUsuarioController from '../src/controllers/usuario/listar-usuario'
+import CriarUsuarioController from '../src/controllers/usuario/criar-usuario';
+import DeletaUsuarioController from '../src/controllers/usuario/deletar-usuario';
+import sequelize from "../src/database";
+import { initializeDatabaseAndServer } from '../src/config/initializeDatebaseAndServer'
+
+initializeDatabaseAndServer(sequelize)
+// Sincronizar o banco de dados e iniciar o servidor
+
+
+// test('Listar dados de usuario com base no ID', async () => {
+//   const request = {
+//     params: { id: 1 },
+//     body: {},
+//   }
+
+//   const controleer = new ListarUsuarioController()
+//   const result = await controleer.handle(request)
+
+//   expect(result.body.get()).toMatchObject({
+//     id: 1,
+//     nome: 'João da Silva',
+//     email: 'joao.silva@dominio.com',
+//     senha: '$2b$10$HA/r8c/TCb56SsJDy2PesuHlaK0dh.3h5ON1tUKu0Oy3.FQv.x2x.',
+//   })
+
+// });
+
+test('Criar usuario', async () => {
+  const request = {
+    params: {},
+    body: {
+      nome: "João da Silva",
+      email: "teste@gmail.com",
+      senha: "senha"
+    }
+  }
+
+  const controllerCria = new CriarUsuarioController()
+  const response = await controllerCria.handle(request)
+
+  const controleerListar = new ListarUsuarioController()
+  const usuarioCriado = await controleerListar.handle({
+    params: { id: response.body.dataValues.id },
+    body: {}
+  })
+
+
+
+  const values = usuarioCriado.body.get()
+
+  expect(response.body.get()).toMatchObject({
+    id: values.id,
+    email: values.email,
+    nome: values.nome,
+    senha: values.senha,
+  })
+
+})
+
+
