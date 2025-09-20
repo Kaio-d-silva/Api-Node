e8664c735967aad2fed33074332a1c10c19c96e3
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-09-19 23:00:41 -0400

    ativando alteração das tabelas

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,5 +11,5 @@ export const ENV = {
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || '',
   JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
   API_VERSION: process.env.API_VERSION || 'v1',
-  UPDATE_MODEL: process.env.UPDATE_MODEL || false,
+  UPDATE_MODEL: process.env.UPDATE_MODEL || true,
 };
