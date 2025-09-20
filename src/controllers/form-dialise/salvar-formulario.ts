a4932ec2bae183dc5b36ee10f81c14df39659835
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-09-19 23:00:10 -0400

    validação de campos

--- src/controllers/form-dialise/salvar-formulario.ts
+++ src/controllers/form-dialise/salvar-formulario.ts
@@ -4,9 +4,27 @@ import FormularioDialise from "../../models/formularioDialise-model";
 class SalvarDadosDialise implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
+
+            interface FormularioDialiseData {
+                data_prenchimento: string;
+                data: string;
+                hora_inicio: string;
+                hora_fim: string;
+                solucao_utilizada: string;
+                dreno_inicial: string;
+                UF_total: number;
+                tpm: number;
+                aspecto_liquido_id: number;
+                peso_pre_dialise: number;
+                peso_pos_dialise: number;
+                pressao_diastolica: number;
+                pressao_sistolica: number;
+                glicemia: number;
+                anotacoes?: string | null;
+            }
+
             const {
                 data_prenchimento,
-                hora_preenchimento,
                 data,
                 hora_inicio,
                 hora_fim,
@@ -20,14 +38,43 @@ class SalvarDadosDialise implements Controller {
                 pressao_diastolica,
                 pressao_sistolica,
                 glicemia,
-                anotacoes,
-            } = httpRequest.body
+                anotacoes = null,
+            }: FormularioDialiseData = httpRequest.body
 
+            const requiredFields = {
+                data_prenchimento,
+                data,
+                hora_inicio,
+                hora_fim,
+                solucao_utilizada,
+                dreno_inicial,
+                UF_total,
+                tpm,
+                aspecto_liquido_id,
+                peso_pre_dialise,
+                peso_pos_dialise,
+                pressao_diastolica,
+                pressao_sistolica,
+                glicemia,
+            }
 
-            console.log("Criando registro de dialise")
+            for (const [field, value] of Object.entries(requiredFields)) {
+                if (value === undefined || value === null || value === '') {
+                    return {
+                        statusCode: 400,
+                        body: { message: `O campo ${field} é obrigatório.` }
+                    }
+                }
+                if(typeof field === 'number' && typeof value !== 'number') {
+                    return {
+                        statusCode: 400,
+                        body: { message: `O campo ${field} deve ser um número.` }
+                    }
+                }
+            }
+
             const formulario = await FormularioDialise.create({
                 data_prenchimento,
-                hora_preenchimento,
                 data,
                 hora_inicio,
                 hora_fim,
@@ -51,7 +98,6 @@ class SalvarDadosDialise implements Controller {
 
 
         } catch (error: any) {
-            console.log("entrou do catch")
             return {
                 statusCode: 500,
                 body: { error: error.message }
--- src/controllers/form-glicemia/salvar-formulario.ts
+++ src/controllers/form-glicemia/salvar-formulario.ts
@@ -1,13 +1,27 @@
+import { log } from "console";
 import { Controller, HttpRequest, HttpResponse } from "../../interfaces";
 import FormularioGlicemia from "../../models/formularioGlicemia-model";
 
 class SalvarDadosGlicemia implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
+
+
+            interface FormularioGlicemiaData {
+                data_preenchimento: string;
+                data_formulario: string;
+                glicemia_jejum: number;
+                glicemia_pre_almoco: number;
+                glicemia_apos_almoco: number;
+                glicemia_pre_janta: number;
+                glicemia_apos_janta: number;
+                paciente_id: number;
+                anotacoes?: string | null;
+            } 
+
             const {
-                data_prenchimento,
-                hora_preenchimento,
-                data,
+                data_preenchimento,
+                data_formulario,
                 glicemia_jejum,
                 glicemia_pre_almoco,
                 glicemia_apos_almoco,
@@ -15,14 +29,39 @@ class SalvarDadosGlicemia implements Controller {
                 glicemia_apos_janta,
                 paciente_id,
                 anotacoes = null,
-            } = httpRequest.body
+            }: FormularioGlicemiaData = httpRequest.body
 
+            const requiredFields = {
+                data_preenchimento,
+                data_formulario,
+                glicemia_jejum,
+                glicemia_pre_almoco,
+                glicemia_apos_almoco,
+                glicemia_pre_janta,
+                glicemia_apos_janta,
+                paciente_id,
+            }
+            console.log("requiredFields", requiredFields)
+            const teste = {teste : "teste"}
 
-            console.log("Criando registro de dialise")
+            for (const [field, value] of Object.entries(requiredFields)) {
+                if (value === undefined || value === null || value === '') {
+                    return {
+                        statusCode: 400,
+                        body: { message: `O campo ${field} é obrigatório.` }
+                    }
+                }
+                if(typeof field === 'number' && typeof value !== 'number') {
+                    return {
+                        statusCode: 400,
+                        body: { message: `O campo ${field} deve ser um número.` }
+                    }
+                }
+            }
+
             const formulario = await FormularioGlicemia.create({
-                data_prenchimento,
-                hora_preenchimento,
-                data,
+                data_preenchimento,
+                data_formulario,
                 glicemia_jejum,
                 glicemia_pre_almoco,
                 glicemia_apos_almoco,
@@ -39,7 +78,6 @@ class SalvarDadosGlicemia implements Controller {
 
 
         } catch (error: any) {
-            console.log("entrou do catch")
             return {
                 statusCode: 500,
                 body: { error: error.message }
